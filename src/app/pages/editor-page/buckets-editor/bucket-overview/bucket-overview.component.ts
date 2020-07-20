8eaf3074831943d51f034f515d8a12f77dcf1881
simon <simon.omer1@gmail.com>
2020-07-20 22:52:23 +0300

    modified bucket name to piping

--- src/app/pages/editor-page/buckets-editor/bucket-overview/bucket-overview.component.ts
+++ src/app/pages/editor-page/buckets-editor/bucket-overview/bucket-overview.component.ts
@@ -3,6 +3,8 @@ import {ConnectionsService} from '../../../../services/connections.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {BucketsManagementService} from '../../../../services/contexts.service/management.services/buckets.management.service';
 import {IBucket} from '../../../../models/bucket';
+import {Observable} from 'rxjs';
+import {find, first, switchMap, take, map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-bucket-overview',
@@ -12,16 +14,16 @@ import {IBucket} from '../../../../models/bucket';
 export class BucketOverviewComponent implements OnInit {
 
   bucketId: string;
-  currentBucket: IBucket;
+  currentBucket$: Observable<IBucket>;
 
-  constructor(private connectionsService: ConnectionsService,
+  constructor(private bucketsManagementService: BucketsManagementService,
+              private connectionsService: ConnectionsService,
               private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(async paramsMap => {
       this.bucketId = paramsMap.get('bucketId');
-      const bucket = await this.connectionsService.getForm(this.bucketId);
-      this.currentBucket = bucket;
+      this.currentBucket$ = this.bucketsManagementService.buckets$.pipe(map(buckets => buckets.find(bucket => bucket._id === this.bucketId)));
     });
   }
 
